7521e9da8b74db50ed3895de951f48728546f793
Kartik Kathrotiya <kartikkathrotiya0542@gmail.com>
2026-02-27 12:12:20 +0530

    Fix cookie for production (sameSite none, secure true)

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -137,10 +137,10 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
+        secure: true,
         //sameSite: "lax",
         path: "/",
-        secure: false,
       })
       .json({
         message: `Welcome back ${user.fullname}`,
